b62b84cb5db85427d1f764f1ab1b1c58cc5a70b1
Vinicius Castro <vinicius1595@gmail.com>
2020-05-28 21:03:30 -0300

    implements operation

--- main.py
+++ main.py
@@ -2,32 +2,39 @@
 import kivy   
         
 from kivy.app import App  
-       
+from kivy.core.window import Window
 kivy.require('1.9.0')  
   
 from kivy.uix.gridlayout import GridLayout 
 
 from kivy.config import Config 
+
+Window.size = (300, 400)
+Window.clearcolor = (0.5,0.5,0.5,1)
   
-Config.set('graphics', 'width', '300') 
-Config.set('graphics', 'height', '400') 
   
-  
-class CalcGridLayout(GridLayout): 
+class CalculatorFrm(GridLayout): 
     def calculate(self, calculation): 
         if calculation: 
             try: 
                 self.display.text = str(eval(calculation)) 
             except Exception: 
-                self.display.text = "Error"
+                self.display.text = "Erro para calcular"
+
+    def verify(self, count):
+        if ['-','/','+','*',].__contains__(self.display.text[-1:]):
+            return ''
+        else:
+            return count
+
    
-class CalculatorApp(App): 
+class Calculator(App): 
    
     def build(self): 
-        return CalcGridLayout() 
+        return CalculatorFrm() 
    
 def main():
-    calcApp = CalculatorApp()
+    calcApp = Calculator()
     calcApp.run() 
 
 if __name__ == '__main__':
